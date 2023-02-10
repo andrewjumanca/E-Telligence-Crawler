ecc21b30546d94dc21e4384b22ee6df99262efcf
Christopher Ku <chrisjk868@gmail.com>
2023-02-09 21:49:50 -0800

    fixed express module issues

--- crawler-api/app.js
+++ crawler-api/app.js
@@ -3,15 +3,15 @@ import express, { json, urlencoded } from 'express';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
 var app = express();
-
-// view engine setup
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(logger('dev'));
 app.use(json());
@@ -22,20 +22,8 @@ app.use(express.static(join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
 });
 
 export default app;
--- crawler-api/routes/index.js
+++ crawler-api/routes/index.js
@@ -1,4 +1,5 @@
-var express = require('express');
+import express from 'express';
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;

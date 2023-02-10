8045a6669e9f056cb112bd7ef0a6fd5f9eaf3caa
Christopher Ku <chrisjk868@gmail.com>
2023-02-09 23:15:13 -0800

    established connection to db

--- crawler-api/app.js
+++ crawler-api/app.js
@@ -1,4 +1,3 @@
-import createError from 'http-errors';
 import express, { json, urlencoded } from 'express';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
@@ -8,6 +7,7 @@ import { dirname } from 'path';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
+import models from './models.js';
 
 var app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -19,6 +19,10 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  req.models = models;
+  next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ crawler-api/models.js
@@ -0,0 +1,18 @@
+import mongoose, { model, mongo } from "mongoose";
+
+let models = {}
+const uri = 'mongodb+srv://chrisjk868:9usirHc0@crawler.0jer4eh.mongodb.net/?retryWrites=true&w=majority';
+const productSchema = {product_name: String, seller: String, price: String, URL: String};
+const storageSchema = new mongoose.Schema({product_name: String, urls: [productSchema]});
+
+try {
+    await mongoose.connect(uri);
+    console.log('Connected to Mongodb database');
+} catch (error) {
+    console.log(error);
+}
+
+models.Products = mongoose.model('Products', storageSchema);
+console.log('Finished creating models');
+
+export default models;

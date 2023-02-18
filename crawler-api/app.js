d60e7fef920dda3088821b8933b9ee154c7c0afb
mattramirez7 <77175367+mattramirez7@users.noreply.github.com>
2023-02-17 17:34:32 -0800

    Database connected to scraper, data not going through quite yet

--- crawler-api/app.js
+++ crawler-api/app.js
@@ -23,7 +23,12 @@ app.use((req, res, next) => {
   req.models = models;
   next();
 });
+
 app.use('/api/v1', apiv1Router);
 app.use('/users', usersRouter);
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
 
 export default app;

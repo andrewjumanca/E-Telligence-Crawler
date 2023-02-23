cbc7d87b5651bc0abbb50703015f4badcc68f08a
mattramirez7 <77175367+mattramirez7@users.noreply.github.com>
2023-02-22 17:29:57 -0800

    Updated working code

--- eTelligenceCrawler/spiders/googleShoppingSpider.py
+++ eTelligenceCrawler/spiders/googleShoppingSpider.py
@@ -1,19 +1,33 @@
 import scrapy
 import requests
 import json
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 class googleShoppingSpider(scrapy.Spider):
     name = "googleShoppingSpider"
-    start_urls = ["https://www.google.com/search?q=Jordan+Essential+Winter+mens+fleece+hoodie&sxsrf=AJOqlzVcDDvf7V-9QSLWJxt7BoB_o0iXag:1674173548345&source=lnms&tbm=shop&sa=X&ved=2ahUKEwjbw5C97tT8AhVxJ30KHSUaBewQ_AUoAXoECAEQAw&biw=1269&bih=725&dpr=2"]
+
+    def __init__(self, search_query='', **kwargs):
+        super().__init__(**kwargs)
+        self.search_query = search_query
+
+    def start_requests(self):
+        url = f'https://www.google.com/search?q={self.search_query}&source=lnms&tbm=shop'
+        yield scrapy.Request(url=url, callback=self.parse)
+    
 
     def parse(self, response):
-        product_data = {'product_name': "placeholder",
+        product_data = {'product_name': self.search_query,
                     'urls': []}
+
+        queryRegex = convert_string_to_regex(self.search_query)
+        print(queryRegex)
         
-        link_extractor = LinkExtractor()
+        link_extractor = LinkExtractor(allow=(rf'{queryRegex}',))
+
+        # r'.*Jordan\+Essential\+Winter\+Mens\+Fleece\+Hoodie.*'
 
         for link in link_extractor.extract_links(response):
             product_data['urls'].append(link.url)
@@ -24,3 +38,13 @@ class googleShoppingSpider(scrapy.Spider):
                     print('Data sent successfully')
         except Exception as e:
             print(e)
+    
+
+def convert_string_to_regex(string):
+    # Remove leading/trailing white space and replace internal white space with '+'
+    string = re.sub(r'\s+', '+', string.strip())
+    # Add escape slashes to each space in the modified string
+    string = re.sub(r'\+', r'\+', string)
+    # Wrap the modified string in the regex syntax
+    return rf'.*{string}.*'
+    

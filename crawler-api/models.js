ed740b56cc63b4563b43f4328815da7620abf428
Christopher Ku <chrisjk868@gmail.com>
2023-02-22 17:41:03 -0800

    comitting api

--- crawler-api/models.js
+++ crawler-api/models.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 let models = {}
-const uri = 'mongodb+srv://chrisjk868:9usirHc0@crawler.0jer4eh.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://chrisjk868:pa33word@crawler.0jer4eh.mongodb.net/?retryWrites=true&w=majority';
 const productSchema = {product_name: String, seller: String, price: String, URL: String};
 const storageSchema = new mongoose.Schema({product_name: String, urls: [productSchema]});
 
